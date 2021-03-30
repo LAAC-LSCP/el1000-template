211967814d9f3101602db7872cd51fafa4412ac9
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-03-30 12:09:58 +0200

    setup script update

---
+++ .datalad/procedures/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import re
+
+import datalad.api
+from datalad.distribution.dataset import require_dataset
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('path', help = 'path')
+parser.add_argument('--confidential', help = 'setup access to confidential data', action = 'store_true')
+
+args = parser.parse_args()
+
+ds = require_dataset(
+    args.path,
+    check_installed = True,
+    purpose = 'configuration'
+)
+
+datalad.api.siblings(
+    name = 'origin',
+    dataset = ds,
+    action = 'configure',
+    annex_wanted = '(include=*) and (exclude=**/confidential/*)',
+    annex_required = '(include=*) and (exclude=**/confidential/*)'
+)
+
+if args.confidential:
+    url = datalad.api.siblings(name = 'origin', dataset = ds)[0]['url']
+    confidential_url = os.path.splitext(url)[0] + '-confidential.git'
+
+    datalad.api.siblings(
+        name = 'confidential',
+        dataset = ds,
+        action = 'add',
+        url = confidential_url
+    )
+
+    datalad.api.siblings(
+        name = 'confidential',
+        dataset = ds,
+        action = 'configure',
+        annex_wanted = 'include=**/confidential/*',
+        annex_required = 'include=**/confidential/*'
+    )
+
+    datalad.api.siblings(
+        name = 'origin',
+        dataset = ds,
+        action = 'configure',
+        publish_depends = 'confidential'
+    )
+    
+
